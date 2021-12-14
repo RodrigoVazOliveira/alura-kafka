3871f27a7cb291eca4147198ab345e0ba312b03c
Rodrigo Vaz <rodrigovazdeoliveira@gmail.com>
2021-12-14 16:22:54 -0300

    criado servlet

---
+++ service-httpecommerce/src/main/java/dev/rvz/NewOrderServlet.java
@@ -0,0 +1,15 @@
+package dev.rvz;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class NewOrderServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        
+        super.doPost(req, resp);
+    }
+}
