8e213f00992b0424b195e8c35c39874963451599
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-12-20 04:47:27 -0300

    criado serialização customizada

--- common-kafka/src/main/java/dev/rvz/models/Message.java
+++ common-kafka/src/main/java/dev/rvz/models/Message.java
@@ -7,9 +7,17 @@ public class Message<T> {
     private final CorrelationId id;
     private final T payload;
 
-    Message(CorrelationId id, T payload) {
+    public Message(CorrelationId id, T payload) {
         this.id = id;
         this.payload = payload;
+    }
+
+    public CorrelationId getId() {
+        return id;
+    }
+
+    public T getPayload() {
+        return payload;
     }
 
     @Override
