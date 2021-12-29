90f45d39574774e8144f1835c5efd7b9f80503bf
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-12-29 08:52:10 -0300

    Usar threads em reading reports

--- common-kafka/src/main/java/dev/rvz/consummers/ConsumerService.java
+++ common-kafka/src/main/java/dev/rvz/consummers/ConsumerService.java
@@ -3,10 +3,11 @@ package dev.rvz.consummers;
 import dev.rvz.models.Message;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
+import java.io.IOException;
+
 public interface ConsumerService<T> {
 
     String getTopic();
     String getGroupId();
-
-    void parse(ConsumerRecord<String, Message<String>> record);
+    void parse(ConsumerRecord<String, Message<T>> record) throws IOException;
 }
--- service-reading-report/src/main/java/dev/rvz/consummers/ReadingReportService.java
+++ service-reading-report/src/main/java/dev/rvz/consummers/ReadingReportService.java
@@ -3,32 +3,37 @@ package dev.rvz.consummers;
 import dev.rvz.io.IO;
 import dev.rvz.models.Message;
 import dev.rvz.models.serializables.User;
-import dev.rvz.services.KafkaService;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
-import java.util.HashMap;
-import java.util.concurrent.ExecutionException;
 
-public class ReadingReportService {
+public class ReadingReportService implements ConsumerService<User> {
 
     private static final Path SOURCE = new File("src/main/resources/report.txt").toPath();
 
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
-        ReadingReportService readingReportService = new ReadingReportService();
-        KafkaService<User> kafkaService = new KafkaService<>(ReadingReportService.class.getSimpleName(), "ECOMMERCE_USER_GENERATE_READING_REPORT",
-                readingReportService::parse, new HashMap<>());
-        kafkaService.run();
+    public static void main(String[] args) {
+        new ServiceRunner<>(ReadingReportService::new).start(5);
     }
 
-    void parse(ConsumerRecord<String, Message<User>> consumerRecord) throws IOException {
-        System.out.println("Processing report for " + consumerRecord.value());
+    @Override
+    public String getTopic() {
+        return "ECOMMERCE_USER_GENERATE_READING_REPORT";
+    }
 
-        File target = new File(consumerRecord.value().getPayload().getPath());
+    @Override
+    public String getGroupId() {
+        return ReadingReportService.class.getSimpleName();
+    }
+
+    @Override
+    public void parse(ConsumerRecord<String, Message<User>> record) throws IOException {
+        System.out.println("Processing report for " + record.value());
+
+        File target = new File(record.value().getPayload().getPath());
         IO.copyTo(SOURCE, target);
-        IO.append(target, "Created for " + consumerRecord.value().getPayload().getUuid());
+        IO.append(target, "Created for " + record.value().getPayload().getUuid());
 
         System.out.println("File created: " + target.getAbsolutePath());
     }
